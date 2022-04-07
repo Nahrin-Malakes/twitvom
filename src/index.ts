7d68d8736c394f1919f357bd8dcd83300700ab4f
NahrinMalakes <95406844+ShohamMalakes@users.noreply.github.com>
2022-04-07 14:40:33 +0300

    second

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import { WeatherRouter } from "./routes/weather";
-import dotenv from "dotenv";
-dotenv.config();
 
 const app = express();
 
 app.use("/weather", WeatherRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server started on ${process.env.PORT}`);
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server started on ${process.env.PORT || 4000}`);
 });
 
--- src/routes/weather.ts
+++ src/routes/weather.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import OpenWeatherMap from "openweathermap-ts";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const WeatherRouter = Router();
 
 WeatherRouter.get("/", async (_, res) => {
   const openWeather = new OpenWeatherMap({
-    apiKey: process.env.API_KEY,
+    apiKey: "5581e5b924854e69fe171f17c94982c2",
     language: "he",
     units: "metric",
   });
